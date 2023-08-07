3bbf680106a2cbee5fcf7f42ff79f611c8888c0b
Haziel Revert <hazielrevert@gmail.com>
2023-08-07 11:20:29 -0300

    se crea un logging con admin admin

--- js/main.js
+++ js/main.js
@@ -1,4 +1,63 @@
 
+//variables
+
+const usuario = "admin";
+const clave = "admin";
+var usuario_correcto = "0";
+var clave_correcta = "0";
+var intento = "0";
+
+
+
+window.alert("Sig in");
+
+//ingresar usuario
+
+while(usuario_correcto ==0)
+{
+let user = prompt("ingrese usuario")
+if (user == usuario)
+{
+   usuario_correcto = 1;
+   while(clave_correcta !=1)
+   {
+      let pass = prompt("ingrese clave")
+         if (pass==clave )
+         {
+            clave_correcta = 1;
+            window.alert("Bienvenid@ "+user);
+
+         }
+         else{
+            alert("Clave Incorrecta");
+            intento = intento +1;
+         if( intento == 3)
+               {
+            //return;
+            
+            window.alert("algo");
+         }
+          
+            
+         }
+   }
+}
+else{
+         alert("Usuario incorrecto");
+         
+   }
+
+
+
+}
+
+
+
+
+ 
+
+
+/*
 window.alert("armar una frase sin sentido");
 var palabra = prompt ("ingresar palabras, para salir ingresar .");
 var frase = palabra;
@@ -16,3 +75,16 @@ while(palabra != '.')
        
 
     }
+
+
+    //funcion de impresion en consola
+    alert (FraseCompleta)
+
+    //cantidad de palabras ingresadas
+    var cantidadPalabras=0 ;
+    for (let i of  FraseCompleta){
+      
+    }
+
+
+*/
